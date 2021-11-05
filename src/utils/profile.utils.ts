365279911c66d0de2b9f1d3190e2309c94369531
Gérôme Grignon <gerome.grignon.lp2@gmail.com>
2021-11-05 21:42:08 +0100

    fix: image property populated accordingly

--- src/utils/profile.utils.ts
+++ src/utils/profile.utils.ts
@@ -4,7 +4,7 @@ import { Profile } from '../models/profile.model';
 const profileMapper = (user: any, username: string | undefined): Profile => ({
   username: user.username,
   bio: user.bio,
-  image: user.bio,
+  image: user.image,
   following: username
     ? user?.followedBy.some((followingUser: Partial<User>) => followingUser.username === username)
     : false,
