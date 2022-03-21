6f286b394aa3cee9656b424bcee532c1437d0949
geromegrignon <gerome.grignon.lp2@gmail.com>
2022-03-22 00:19:21 +0100

    fix: add username to token

--- src/utils/token.utils.ts
+++ src/utils/token.utils.ts
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
+import { User } from '../models/user.model';
 
-const generateToken = (payload: string): string =>
-  jwt.sign({ email: payload }, process.env.JWT_SECRET || 'superSecret', { expiresIn: '60d' });
+const generateToken = ({ email, username }: Partial<User>): string =>
+  jwt.sign({ email, username }, process.env.JWT_SECRET || 'superSecret', { expiresIn: '60d' });
 
 export default generateToken;
